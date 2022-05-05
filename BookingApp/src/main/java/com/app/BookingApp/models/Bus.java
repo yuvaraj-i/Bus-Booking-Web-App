795eb7e0e13311675a06b2cd0be6e36b94e08926
yuvaraj-i <yuvarajyosef@gmail.com>
2022-05-05 17:10:08 +0530

    [update] updated to lombok for getter & setters

--- BookingApp/src/main/java/com/app/BookingApp/models/Bus.java
+++ BookingApp/src/main/java/com/app/BookingApp/models/Bus.java
@@ -8,8 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table
+@Getter
+@Setter
 public class Bus {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -26,120 +31,5 @@ public class Bus {
     private String numberPlateDeatails;
     private boolean isAvaliable = true;
     private boolean isDestinationReached = false;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBusName() {
-        return this.busName;
-    }
-
-    public void setBusName(String busName) {
-        this.busName = busName;
-    }
-
-    public int getNumberOfSeats() {
-        return this.numberOfSeats;
-    }
-
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
-
-    public String getType() {
-        return this.type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getStartLocation() {
-        return this.startLocation;
-    }
-
-    public void setStartLocation(String startLocation) {
-        this.startLocation = startLocation;
-    }
-
-    public String getEndLocation() {
-        return this.endLocation;
-    }
-
-    public void setEndLocation(String endLocation) {
-        this.endLocation = endLocation;
-    }
-
-    public String getBoardingPoint() {
-        return this.boardingPoint;
-    }
-
-    public void setBoardingPoint(String boardingPoint) {
-        this.boardingPoint = boardingPoint;
-    }
-
-    public String getDropingPoint() {
-        return this.dropingPoint;
-    }
-
-    public void setDropingPoint(String dropingPoint) {
-        this.dropingPoint = dropingPoint;
-    }
-
-    public LocalTime getStartTime() {
-        return this.startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return this.endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public boolean isIsAvaliable() {
-        return this.isAvaliable;
-    }
-
-    public boolean getIsAvaliable() {
-        return this.isAvaliable;
-    }
-
-    public void setIsAvaliable(boolean isAvaliable) {
-        this.isAvaliable = isAvaliable;
-    }
-
-
-    public boolean isIsDestinationReached() {
-        return this.isDestinationReached;
-    }
-
-    public boolean getIsDestinationReached() {
-        return this.isDestinationReached;
-    }
-
-    public void setIsDestinationReached(boolean isDestinationReached) {
-        this.isDestinationReached = isDestinationReached;
-    }
-
-
-    public String getNumberPlateDetails() {
-        return this.numberPlateDeatails;
-    }
-
-    public void setNumberPlateDetails(String numberPlateDeatails) {
-        this.numberPlateDeatails = numberPlateDeatails;
-    }
-
 
 }
--- BookingApp/src/main/java/com/app/BookingApp/models/MyUser.java
+++ BookingApp/src/main/java/com/app/BookingApp/models/MyUser.java
@@ -8,8 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "user")
+@Getter
+@Setter
 public class MyUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,59 +26,9 @@ public class MyUser {
     private int age;
     private String password;
 
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobileNumber() {
-        return this.mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getEmailAddress() {
-        return this.emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return this.dateOfBirth;
-    }
-
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
         this.setAge(dateOfBirth);
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public void setAge(int age) { }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     private void setAge(LocalDate age) {
